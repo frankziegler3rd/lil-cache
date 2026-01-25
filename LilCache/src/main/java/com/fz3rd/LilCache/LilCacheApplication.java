c0972f0f591bf16587ddc566959c45d66b5f18dc
Frank Ziegler <ziegle15@students.rowan.edu>
2026-01-25 14:54:18 -0500

    converted pure Java to Spring

---
+++ LilCache/src/main/java/com/fz3rd/LilCache/LilCacheApplication.java
@@ -0,0 +1,13 @@
+package com.fz3rd.LilCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilCacheApplication.class, args);
+	}
+
+}
---
+++ LilCache/src/test/java/com/fz3rd/LilCache/LilCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fz3rd.LilCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

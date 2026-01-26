9ed8f6a2c27da3ea5f355386eb5f3e8196f13839
Frank Ziegler <ziegle15@students.rowan.edu>
2026-01-26 01:20:25 -0500

    added service class to keep controller thin

---
+++ LilCache/src/main/java/com/fz3rd/LilCache/service/CacheService.java
@@ -0,0 +1,43 @@
+/**
+ * service class to perform cache business logic and keep controller thin
+ *
+ * @author Frank Ziegler
+ * @version 1.0.0
+ */
+
+import org.springframework.stereotype.Service;
+import com.fz3rd.LilCache.cache.Cache;
+import com.fz3rd.LilCache.cache.InMemCache;
+import com.fz3rd.LilCache.cache.Entry;
+import com.fz3rd.LilCache.cache.eviction.EvictionPolicy;
+import com.fz3rd.LilCache.cache.eviction.LRUPolicy;
+
+@Service
+public class CacheService {
+    
+    private final Cache<String, Entry<String>> cache;
+
+    public CacheService(EvictionPolicy<String> ep, int capacity) {
+        cache = new InMemCache<>(ep, capacity);
+    }
+
+    public String get(String key) {
+        return cache.get(key) == null ? null : entry.getValue();
+    }
+
+    public void put(String key, String value) {
+        cache.put(key, value);
+    }
+
+    public void delete(String key) {
+        cache.delete(key);
+    }
+
+    public void deleteAll() {
+        cache.deleteAll();
+    }
+
+    public int size() {
+        return cache.size();
+    }
+}
